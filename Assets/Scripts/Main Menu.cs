3d148183e8afff41fcec12bdb7de6757ccbe27c5
KonKonKitsune-SenkoDayo <saikomiyashiro@gmail.com>
2026-02-11 09:55:49 +0800

    The Main Menu is gucci
    AAAAAAAAAAAAIIIIIIGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
